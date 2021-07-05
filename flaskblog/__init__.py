74b5761072603b87d8bb96422528ca1d80682af0
Andrei0991 <aalupului1990@gmail.com>
2021-07-05 16:10:32 +0300

    Blog app commited

---
+++ flaskblog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+from flask_bcrypt import Bcrypt
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f1743f5bb049f24e69ffeee14dfc8e6f'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'andrei'
+app.config['MYSQL_PASSWORD'] = 'pulamea123'
+app.config['MYSQL_DB'] = 'users_db'
+db = MySQL(app)
+bcrypt = Bcrypt(app)
+
+from flaskblog import routes
