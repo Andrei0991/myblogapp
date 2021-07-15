316fdecb3099c506ce79307731981a48856ed13b
Andrei0991 <aalupului1990@gmail.com>
2021-07-15 16:54:11 +0300

    comments

--- flaskblog/posts/routes.py
+++ flaskblog/posts/routes.py
@@ -10,9 +10,17 @@ postings = Blueprint('postings', __name__)
 def posts():
 	if 'loggedin' in session:
 		cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
-		cur.execute(f"SELECT * FROM posts LEFT JOIN users ON users.idUser = posts.idUser LEFT JOIN comments ON posts.idPost = comments.idPost WHERE statusPost = 1 AND statusUser = 1 ORDER BY posts.datePosted DESC")
+		cur.execute(f"SELECT * FROM posts LEFT JOIN users ON users.idUser = posts.idUser WHERE statusPost = 1 AND statusUser = 1 ORDER BY posts.datePosted DESC")
 		post_details = cur.fetchall()
-	return render_template('posts.html', post_details = post_details, username = session['username'])
+		cur.execute(f"SELECT * FROM comments LEFT JOIN users ON users.idUser = comments.idUser WHERE statusUser = 1 AND statusComment = 1 ORDER BY comments.dateComment ASC")
+		comments = cur.fetchall()
+		commentsAdded = {}
+		for post in post_details:
+			commentsAdded[post['idPost']] = []
+		for comment in comments:
+			commentsAdded[comment['idPost']].append(comment)
+	return render_template('posts.html', post_details = post_details, username = session['username'], commentsAdded = commentsAdded)
+
 
 @postings.route('/delete/<id_post>', methods=['GET', 'POST'])
 def delete(id_post):
@@ -21,9 +29,10 @@ def delete(id_post):
 	db.connection.commit()
 	return redirect (url_for('postings.posts'))
 
+
 @postings.route('/update/<id_post>', methods=['GET', 'POST'])
 def update(id_post):
-	if request.method == "POST":
+	if request.method == "POST" and 'loggedin' in session:
 		title = request.form['title']
 		content = request.form['content']
 		cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -31,7 +40,7 @@ def update(id_post):
 		db.connection.commit()
 		return redirect(url_for('postings.posts'))
 	cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-	cursor.execute('SELECT * FROM posts WHERE idPost = %s', (id_post,))
+	cursor.execute(f'SELECT * FROM posts WHERE idPost = %s', (id_post,))
 	post = cursor.fetchone()
 	return render_template('update.html', post = post)
 
@@ -42,16 +51,19 @@ def comments(id_post):
 		idPost = request.form['idPost']
 		context = request.form['context']
 		cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute("INSERT INTO comments (idPost, context) VALUES ('{0}', '{1}')".format(idPost, context, id_post))
+		cursor.execute("INSERT INTO comments (idPost, idUser, context) VALUES ('{0}', '{1}', '{2}')".format(idPost, session['id'], context, id_post))
 		db.connection.commit()
 		cursor.close()
 		return redirect(url_for('postings.posts'))
-	cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-	cursor.execute("SELECT * FROM comments WHERE idPost = {id_post} AND statusComment = 1")
-	post = cursor.fetchone()
-	return render_template('posts.html', post = post)
 
 
+
+@postings.route('/deleteComments/<id_post>', methods=['GET', 'POST'])
+def deleteComments(id_post):
+	cur = db.connection.cursor(MySQLdb.cursors.DictCursor)
+	cur.execute(f'DELETE FROM comments WHERE idComment = {id_post} AND statusComment = 1')
+	db.connection.commit()
+	return redirect (url_for('postings.posts'))
 
 
 
